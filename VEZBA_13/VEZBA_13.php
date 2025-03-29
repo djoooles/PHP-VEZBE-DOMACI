de3561a31b4c37483316ab6d889897bf0ee0982b
Djordje Stojkovic <djole8793@gmail.com>
2025-03-29 19:06:26 +0100

    Add files via upload

---
+++ VEZBA_13/VEZBA_13.php
@@ -0,0 +1,28 @@
+<?php
+
+    
+    $Ime = "administrator";
+
+    $Lozinka = "mojaSifraJeSigurna";
+
+    if($Ime == strtolower("ADMINISTRAtor"))
+    {
+        echo ucfirst("dobrodosao administratore!");
+    }
+
+    echo " Time is " . date("h:i:sa"), " it's a nightime! Get some sleep!";
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+?>
