ec7910eefb5d5d18bd0295ce579ea48e90664827
Djordje Stojkovic <djole8793@gmail.com>
2025-04-14 13:38:22 +0200

    Add files via upload

---
+++ petlje/petlje.php
@@ -0,0 +1,26 @@
+<?php
+        $trailers = ["Jordi","Mick","Hugo"];
+
+        foreach($trailers as $trailer)
+        {
+            if($trailer == "Hugo")
+            {
+                continue;
+            }
+            elseif($trailer == "Mick")
+            {
+                continue;
+            }
+            echo $trailer;
+        }
+
+
+
+
+
+
+
+
+
+
+?>
