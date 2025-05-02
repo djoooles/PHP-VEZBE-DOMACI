4271125f653c256554d380b014e2b0ea39570b91
Djordje Stojkovic <djole8793@gmail.com>
2025-05-02 10:24:19 +0200

    Update vezbanje.php

--- funkcije_12/vezbanje.php
+++ funkcije_12/vezbanje.php
@@ -4,7 +4,7 @@ function nizBrojeva($brojevi)
     foreach($brojevi as $broj)
     if($broj > 10)
     {
-        echo $broj;
+        echo $broj. "<br>";
     }
 }
 nizBrojeva([11,2,3,16,77,44,2,88]);
