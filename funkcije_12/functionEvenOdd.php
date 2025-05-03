6331360d09fe526984a53c1201970bd34ca11198
Djordje Stojkovic <djole8793@gmail.com>
2025-05-03 05:48:30 -0500

    Add files via upload

---
+++ funkcije_12/functionEvenOdd.php
@@ -0,0 +1,16 @@
+<?php
+function proveraBroja($broj)
+{
+    if($broj % 2 == 0)
+    {
+    echo "$broj Je paran broj!";
+    }
+    else
+    {
+        echo "$broj je neparan!"; 
+    }
+
+}
+proveraBroja(1);
+
+?>
